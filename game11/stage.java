41f6b029bc6a65ff66b7c221d5b1c3cdfc037835
sho-u2 <21169@hakodate.kosen-ac.jp>
2025-12-16 09:18:43 +0900

    ガワだけ実装

--- game11/stage.java
+++ game11/stage.java
@@ -1,23 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class home here.
+ * Write a description of class stage here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class home extends World
+public class stage extends World
 {
 
     /**
-     * Constructor for objects of class home.
+     * Constructor for objects of class stage.
      * 
      */
-    public home()
+    public stage()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        showText("堀坊の落単を阻止せよ！\ntype 「START」", 300, 300);
-        addObject( new button(), 300, 200 );
+        showText("stage1", 300, 300);
     }
 }
