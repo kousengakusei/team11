c584ddbcead77f90268e3753a25a5039231e3781
HoriYou <21160@hakodate.kosen-ac.jp>
2026-01-20 10:13:19 +0900

    テキストの追加

--- game11/stage1.java
+++ game11/stage1.java
@@ -8,7 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class stage1 extends World
 {
-
+    
+    private int i = 0;
+    private boolean spacePressed = false;
+    
     /**
      * Constructor for objects of class stage1.
      * 
@@ -23,11 +26,74 @@ public class stage1 extends World
         TimerActor timer = new TimerActor(10);
         addObject(timer, 550, 10);
     }
+    
     public void act()
     {
-        if( Greenfoot.isKeyDown( "t" ) ){
-            World stage = new stage2();
-            Greenfoot.setWorld( stage );
+        
+        if (Greenfoot.isKeyDown("space")) { 
+            
+            if (!spacePressed) { 
+                
+                i++; spacePressed = true; 
+            } 
+        } else { 
+            
+            spacePressed = false; 
         }
+
+        switch(i) { 
+            
+            case 0: 
+            showText("ある日、堀坊はいつもの通りに高専に登校していた。", 300, 300); 
+            break; 
+            
+            case 1: 
+            showText("登校中、堀坊はなぜだか胸騒ぎがした。", 300, 300); 
+            break; 
+            
+            case 2: 
+            showText("教室に着いたとき、\nその胸騒ぎの答え合わせが目の前にあった。", 300, 300); 
+            break; 
+            
+            case 3: 
+            showText("なぜなら、今日はレポート提出日だったのだから。", 300, 300); 
+            break;
+            
+            case 4: 
+            showText("レポートを出さなくては落単。", 300, 300); 
+            break;
+            
+            case 5: 
+            showText("今からレポートを作成するにはあまりにも現実的ではない。", 300, 300); 
+            break;
+            
+            case 6: 
+            showText("そこで、堀坊は担当の先生に直談判することにした。", 300, 300); 
+            break;
+            
+            case 7: 
+            showText("しかし、担当の先生は話を聞いてくれない。", 300, 300); 
+            break;
+            
+            case 8: 
+            showText("そこで校長先生に直談判をしようと決めた。", 300, 300); 
+            break;
+            
+            case 9: 
+            showText("何事も、最高権力を行使すれば言うことを聞くはず。", 300, 300); 
+            break;
+            
+            case 10: 
+            showText("なんとか上手く言い訳をして、落単を阻止する。", 300, 300); 
+            break;
+            
+            case 11: 
+            showText("すべては落単を回避するために___", 300, 300); 
+            break;
+            
+            case 12: 
+            Greenfoot.setWorld(new stage2());
+            break;
+        } 
     }
 }
