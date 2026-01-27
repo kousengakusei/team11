79119358d7162c5906c4cc1d3a346c065bc6d5cf
sot-m <21163@hakodate.kosen-ac.jp>
2026-01-27 10:10:13 +0900

    キー入力管理の処理を追加に関する修正

--- game11/home.java
+++ game11/home.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+﻿import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.io.File;
 
 /**
@@ -20,21 +20,9 @@ public class home extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         man = new KeyTypingManager2();
-        man.append("テスト");
-        showText(man.upcoming(), 100, 100);
         showText("堀坊の落単を阻止せよ！", 300, 200);
         showText("press 「s」", 300, 300);
         addObject( new textbox(), 300, 300 );
-        /*
-        try {
-            KeyTypingManager2 manager = new KeyTypingManager2();
-            manager.append("test");
-            showText(String.valueOf(manager.onTyped('t')) , 10, 0);
-            manager.close();
-        } catch (java.io.IOException e) {
-            showText(e.toString() , 0, 0);
-        }
-        // */
     }
     
     public void act()
