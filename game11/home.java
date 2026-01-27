1cb3f0f3e4aa904fe34db6f7bdd2a04411597d5d
sot-m <21163@hakodate.kosen-ac.jp>
2026-01-27 10:11:26 +0900

    エンコードの修正

--- game11/home.java
+++ game11/home.java
@@ -1,4 +1,4 @@
-﻿import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.io.File;
 
 /**
